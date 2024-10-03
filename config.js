c1afa3f75dcb6dc412f406f0a9cbd7ef145a542c
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-10-03 17:39:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=HMk0zI6L#uvvqYSQuE6UVzWx8ltEt3VuqF0KOlPPOdOoi9ZfxlYc",
+MONGODB: process.env.MONGODB || "mongodb://mongo:hIHeNtoAoGDfnZUYiCrIFNZguGRwpTFz@junction.proxy.rlwy.net:58427",//enter mongo db url
 };
